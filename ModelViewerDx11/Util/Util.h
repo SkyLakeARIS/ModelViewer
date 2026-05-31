522cb1589473494beaabc0074d189bd366937f9f
SkyLake <Seobkim15@gmail.com>
2026-05-31 19:59:25 +0900

    Fix: 해시함수 중복 정의 문제 수정

--- ModelViewerDx11/Util/Util.h
+++ ModelViewerDx11/Util/Util.h
@@ -5,7 +5,7 @@ namespace util
 {
     // TODO: 나중에 충돌문제가 발견되면 uint64_t 전환 필요
     // TODO: 좀 더 괜찮은 해시 함수로 교체 검토(xxHash, MurmurHash3, FNV-1a)
-    HashID GetDjb2Hash(const uint8_t* str)
+    inline HashID GetDjb2Hash(const uint8_t* str)
     {
         HashID hash = 5381;
         int32_t c = 0;
@@ -17,7 +17,7 @@ namespace util
         return hash;
     }
 
-    HashID GetDjb2Hash(const int8_t* str)
+    inline HashID GetDjb2Hash(const int8_t* str)
     {
         HashID hash = 5381;
         int32_t c = 0;
